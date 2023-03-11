f5d696865a960a5db4cb91435bfaa76037f7d9c6
giadev25 <argiamahya1602@gmail.com>
2023-03-11 11:45:34 +0700

    learn: controller and route config

---
+++ application/views/contact.php
@@ -0,0 +1,2 @@
+<h1>Halaman contact</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum earum necessitatibus fuga dolorum voluptatibus recusandae, ullam unde sunt, error commodi, voluptatum porro amet ut ab facilis repellat laboriosam doloribus ratione.</p>
